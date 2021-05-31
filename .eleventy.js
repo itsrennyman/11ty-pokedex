fe6c8d93bc5f8a71c7a9a520e1fd15dd8efd71aa
Renato Pozzi <r.pozzi@bluecube.it>
2021-05-31 17:14:53 +0200

    added tailwindcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (config) {
+  config.addPassthroughCopy({
+    "src/_includes/assets/css/tailwind.css": "./tailwind.css",
+  });
+
   return {
     dir: {
       input: "src",
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{js,njk,html}"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
