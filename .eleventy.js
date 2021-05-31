6637259e7b0620d397dc072fe90b3e00af41eb8f
Renato Pozzi <r.pozzi@bluecube.it>
2021-05-31 19:09:44 +0200

    11ty image configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,35 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ["avif", "jpeg"],
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (config) {
   config.addWatchTarget("./_tmp/tailwind.css");
   config.addPassthroughCopy({ "./_tmp/tailwind.css": "./tailwind.css" });
+  config.addPassthroughCopy("img");
+  config.addNunjucksAsyncShortcode("image", imageShortcode);
+  config.addLiquidShortcode("image", imageShortcode);
+  config.addJavaScriptFunction("image", imageShortcode);
+
+  // UcFirst
+  config.addNunjucksFilter(
+    "ucfirst",
+    (value) => value.charAt(0).toUpperCase() + value.slice(1)
+  );
 
   return {
     dir: {
