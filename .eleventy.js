fcc108e3a6c957a5c45199acf730ae4e71ae97f1
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-01 21:17:48 +0200

    performance improvements

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ const Image = require("@11ty/eleventy-img");
 
 async function imageShortcode(src, alt, sizes) {
   let metadata = await Image(src, {
-    widths: [100, 200, 300],
-    formats: ["avif", "png"],
+    widths: [150, 300, null],
+    formats: ["avif", "png", "svg"],
     urlPath: "/images/",
     outputDir: "./_site/images",
   });
